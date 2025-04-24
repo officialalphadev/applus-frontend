171508c144155c25ff47313b5002b8a537b58a3a
Rizky Maulana <rizkym71338@gmail.com>
2025-04-24 10:02:46 +0700

    first setup

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,11 +1,6 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { motion } from 'framer-motion'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function isHTMLTag(tag: never): tag is keyof typeof motion {
-  return typeof tag === 'string' && tag in motion
 }
